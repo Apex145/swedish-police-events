2386ec0d131aa7f33eb7a8c338709b0c7dec8161
Apex145 <williamdi68@gmail.com>
2026-02-12 12:11:08 +0100

    Created a map that shows the 20 newest events

--- Script/Main.js
+++ Script/Main.js
@@ -1,31 +1,38 @@
-const proxy = "https://cors-anywhere.herokuapp.com/";
 const target = "https://polisen.se/api/events";
+const proxy = `https://api.allorigins.win/get?url=${encodeURIComponent(target)}`;
 const div = document.getElementById("info-div")
+const map = L.map('map').setView([62.0, 15.0], 5)
 
 
-fetch(proxy + target)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+fetch(target)
 .then(resp => resp.json())
 .then(data => {
     console.log(data);
 
     data.slice(0,20).forEach(information => {
-        let name = document.createElement("h3");
 
-        name.textContent = information.name
+        const fullInforamtion = "https://polisen.se" + information.url;
 
-        div.appendChild(name)
-
-        let summary = document.createElement("p")
-
-        summary.textContent = information.summary
-
-        div.appendChild(summary)
-
-        let location = document.createElement("p")
-
-        location.textContent = information.datetime
-
-        div.appendChild(location)
+        if(information.location?.gps) {
+            const coords = information.location.gps.split(',');
+            L.marker(coords).addTo(map).bindPopup(`
+            <div style="font-family: sans-serif;">
+                <h4 style="margin: 0; color: #003399;">${information.type}</h4>
+                <p style="font-size: 12px; margin: 5px 0;"><b>Plats:</b> ${information.location.name}</p>
+                <p style="font-size: 13px;">${information.summary}</p>
+                <small style="color: gray;">${information.datetime}</small>
+                <a href="${fullInforamtion}" target="_blank" style="color: blue; font-weight: bold;">
+                    Läs mer på polisen.se
+                </a>
+            </div>
+        `);
+        }
     });
     
 })
